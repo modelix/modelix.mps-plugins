0147ae8e29855d36988e2538a1d69b12a6386318
Sascha Lisson <sascha.lisson@itemis.de>
2024-02-01 12:04:57 +0100

    refactor: replaced strings with references in CloudView.createPopupActionGroup

--- mps-legacy-sync-plugin/src/main/java/org/modelix/model/mpsplugin/history/CloudView.kt
+++ mps-legacy-sync-plugin/src/main/java/org/modelix/model/mpsplugin/history/CloudView.kt
@@ -41,27 +41,27 @@ class CloudView : JPanel(BorderLayout()) {
         override fun createPopupActionGroup(node: MPSTreeNode): ActionGroup? {
             if (node is CloudRootTreeNode) {
                 return ActionManager.getInstance()
-                    .getAction("org.modelix.model.mpsplugin.plugin.CloudRootGroup_ActionGroup") as ActionGroup
+                    .getAction(org.modelix.model.mpsplugin.plugin.CloudRootGroup_ActionGroup.ID) as ActionGroup
             }
             if (node is ModelServerTreeNode) {
                 return ActionManager.getInstance()
-                    .getAction("org.modelix.model.mpsplugin.plugin.ModelServerGroup_ActionGroup") as ActionGroup
+                    .getAction(org.modelix.model.mpsplugin.plugin.ModelServerGroup_ActionGroup.ID) as ActionGroup
             }
             if (node is CloudNodeTreeNode) {
                 return ActionManager.getInstance()
-                    .getAction("org.modelix.model.mpsplugin.plugin.CloudNodeGroup_ActionGroup") as ActionGroup
+                    .getAction(org.modelix.model.mpsplugin.plugin.CloudNodeGroup_ActionGroup.ID) as ActionGroup
             }
             if (node is RepositoryTreeNode) {
                 return ActionManager.getInstance()
-                    .getAction("org.modelix.model.mpsplugin.plugin.RepositoryGroup_ActionGroup") as ActionGroup
+                    .getAction(org.modelix.model.mpsplugin.plugin.RepositoryGroup_ActionGroup.ID) as ActionGroup
             }
             if (node is CloudBranchTreeNode) {
                 return ActionManager.getInstance()
-                    .getAction("org.modelix.model.mpsplugin.plugin.CloudBranchGroup_ActionGroup") as ActionGroup
+                    .getAction(org.modelix.model.mpsplugin.plugin.CloudBranchGroup_ActionGroup.ID) as ActionGroup
             }
             if (node is CloudBindingTreeNode) {
                 return ActionManager.getInstance()
-                    .getAction("org.modelix.model.mpsplugin.plugin.CloudBindingGroup_ActionGroup") as ActionGroup
+                    .getAction(org.modelix.model.mpsplugin.plugin.CloudBindingGroup_ActionGroup.ID) as ActionGroup
             }
             return null
         }
