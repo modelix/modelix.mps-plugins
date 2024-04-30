0f0fd49a5157f1845f2c445bad13100dc0d3dd2c
Benedek Horváth <bhorvath@itemis.com>
2024-04-30 11:15:28 +0200

    refactor(mps-sync-plugin-lib): remove duplicate log messages
    we log them in SyncServiceImpl anyways

--- mps-sync-plugin-lib/src/main/kotlin/org/modelix/mps/sync/transformation/modelixToMps/initial/ITreeToSTreeTransformer.kt
+++ mps-sync-plugin-lib/src/main/kotlin/org/modelix/mps/sync/transformation/modelixToMps/initial/ITreeToSTreeTransformer.kt
@@ -16,7 +16,6 @@
 
 package org.modelix.mps.sync.transformation.modelixToMps.initial
 
-import mu.KotlinLogging
 import org.modelix.kotlin.utils.UnstableModelixFeature
 import org.modelix.model.api.BuiltinLanguages
 import org.modelix.model.api.IBranch
@@ -35,8 +34,6 @@ import java.util.concurrent.atomic.AtomicReference
 
 @UnstableModelixFeature(reason = "The new modelix MPS plugin is under construction", intendedFinalization = "2024.1")
 class ITreeToSTreeTransformer(private val branch: IBranch, mpsLanguageRepository: MPSLanguageRepository) {
-
-    private val logger = KotlinLogging.logger {}
 
     private val moduleTransformer = ModuleTransformer(branch, mpsLanguageRepository)
 
