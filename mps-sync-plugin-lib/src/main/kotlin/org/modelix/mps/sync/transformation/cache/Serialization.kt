8dcd81037bbc5284e24c9c457e37a924dd36fee8
Benedek Horváth <bhorvath@itemis.com>
2024-05-06 15:04:01 +0200

    feat(mps-sync-plugin-lib): implement further Serializers WIP

--- mps-sync-plugin-lib/src/main/kotlin/org/modelix/mps/sync/transformation/cache/Serialization.kt
+++ mps-sync-plugin-lib/src/main/kotlin/org/modelix/mps/sync/transformation/cache/Serialization.kt
@@ -1,7 +1,5 @@
 package org.modelix.mps.sync.transformation.cache
 
-import jetbrains.mps.smodel.adapter.structure.MetaAdapterFactory
-import org.jetbrains.mps.openapi.language.SLanguage
 import org.jetbrains.mps.openapi.model.SModel
 import org.jetbrains.mps.openapi.model.SModelReference
 import org.jetbrains.mps.openapi.model.SNode
@@ -13,11 +11,12 @@ import org.modelix.kotlin.utils.UnstableModelixFeature
 import org.modelix.mps.sync.mps.util.clone
 import org.modelix.mps.sync.mps.util.getModelixId
 
+@UnstableModelixFeature(reason = "The new modelix MPS plugin is under construction", intendedFinalization = "2024.1")
 abstract class Serializer<T> {
 
     protected abstract val typeDelimiter: String
 
-    protected open val parentSeparator: String? = null
+    protected open val infixSeparator: String? = null
 
     private val serializationPrefix
         get() = "[$typeDelimiter: "
@@ -35,26 +34,20 @@ abstract class Serializer<T> {
     }
 
     // serialization
-
-    protected fun <U> serializeWithParent(
-        serializedChild: String,
-        childToString: String,
-        parent: U?,
-        parentSerializer: Serializer<U>,
-    ): String {
-        requireNotNull(parent) { "$typeDelimiter ($childToString)'s parent is null." }
-        val serializedParent = parentSerializer.serialize(parent)
-        return "$serializedChild$parentSeparator$serializedParent"
+    protected fun <U> serializeWithRhs(serializedLhs: String, lhs: T, rhs: U?, rhsSerializer: Serializer<U>): String {
+        requireNotNull(rhs) { "$typeDelimiter ($lhs)'s right-hand-side value is null." }
+        val serializedRhs = rhsSerializer.serialize(rhs)
+        return serialize("$serializedLhs$infixSeparator$serializedRhs")
     }
 
     // deserialization
-
     protected fun extractValue(from: String) = from.replace(serializationPrefix, "").replace(serializationSuffix, "")
 
-    protected fun splitIntoParentAndChild(from: String, parentSeparator: String): List<String> {
+    protected fun splitIntoLhsAndRhs(from: String): List<String> {
+        requireNotNull(infixSeparator) { "Infix separator cannot be null." }
         val serializedValue = extractValue(from)
-        val split = serializedValue.split(parentSeparator)
-        require(split.size == 2) { "Serialized value ($from) cannot be split into two parts via its parent separator ($parentSeparator)." }
+        val split = serializedValue.split(infixSeparator!!)
+        require(split.size == 2) { "Serialized value ($from) cannot be split into two parts via its infix separator ($infixSeparator)." }
         return split
     }
 }
@@ -80,16 +73,15 @@ internal class SModuleSerializer(private val repository: SRepository? = null) : 
 internal class SModelSerializer(repository: SRepository? = null) : Serializer<SModel>() {
 
     override val typeDelimiter = "SModel"
-    override val parentSeparator = ", parentModule: "
+    override val infixSeparator = ", parentModule: "
 
     private val parentSerializer = SModuleSerializer(repository)
 
     // [SModel: ID, parentModule: [see SModuleSerializer.serialize(module)]]
-    override fun serialize(it: SModel) =
-        serializeWithParent(it.getModelixId(), it.toString(), it.module, parentSerializer)
+    override fun serialize(it: SModel) = serializeWithRhs(it.getModelixId(), it, it.module, parentSerializer)
 
     override fun deserialize(from: String): SModel? {
-        val split = splitIntoParentAndChild(from, parentSeparator)
+        val split = splitIntoLhsAndRhs(from)
 
         val parentId = split[1]
         val parent = parentSerializer.deserializeNotNull(parentId)
@@ -103,16 +95,15 @@ internal class SModelSerializer(repository: SRepository? = null) : Serializer<SM
 internal class SNodeSerializer(repository: SRepository? = null) : Serializer<SNode>() {
 
     override val typeDelimiter = "SNode"
-    override val parentSeparator = ", parentModel: "
+    override val infixSeparator = ", parentModel: "
 
     private val parentSerializer = SModelSerializer(repository)
 
     // [SModel: ID, parentModel: [see SModelSerializer.serialize(model)]
-    override fun serialize(it: SNode) =
-        serializeWithParent(it.getModelixId(), it.toString(), it.model, parentSerializer)
+    override fun serialize(it: SNode) = serializeWithRhs(it.getModelixId(), it, it.model, parentSerializer)
 
     override fun deserialize(from: String): SNode? {
-        val split = splitIntoParentAndChild(from, parentSeparator)
+        val split = splitIntoLhsAndRhs(from)
 
         val parentId = split[1]
         val parent = parentSerializer.deserializeNotNull(parentId)
@@ -134,9 +125,10 @@ internal class SModelReferenceSerializer(private val repository: SRepository? = 
         requireNotNull(repository) { "Cannot serialize, if repository is null." }
         val model = it.resolve(repository)
         val serializedModel = modelSerializer.serialize(model)
-        return this.serialize(serializedModel)
+        return serialize(serializedModel)
     }
 
+    // TODO maybe we have to deserialize it directly in the model, where it was used. Otherwise the ModelChangeListener will not work correctly for model imports
     override fun deserialize(from: String): SModelReference? {
         val serializedModel = extractValue(from)
         val model = modelSerializer.deserialize(serializedModel)
@@ -158,9 +150,10 @@ internal class SModuleReferenceSerializer(private val repository: SRepository? =
         val module = it.resolve(repository)
         requireNotNull(module) { "Cannot serialize module reference ($it), because resolved Module is null." }
         val serializedModule = moduleSerializer.serialize(module)
-        return this.serialize(serializedModule)
+        return serialize(serializedModule)
     }
 
+    // TODO maybe we have to deserialize it directly in the model, where it was used. Otherwise the ModelChangeListener will not work correctly for devkit/language/module dependencies
     override fun deserialize(from: String): SModuleReference? {
         val serializedModule = extractValue(from)
         val module = moduleSerializer.deserialize(serializedModule)
@@ -169,22 +162,79 @@ internal class SModuleReferenceSerializer(private val repository: SRepository? =
 }
 
 @UnstableModelixFeature(reason = "The new modelix MPS plugin is under construction", intendedFinalization = "2024.1")
-internal class LanguageSerializer(repository: SRepository? = null) : Serializer<SLanguage>() {
+internal class ModelWithModelReferenceSerializer(repository: SRepository? = null) :
+    Serializer<ModelWithModelReference>() {
 
-    override val typeDelimiter = "Language"
+    override val typeDelimiter = "ModelWithModelReference"
+    override val infixSeparator = ", modelReference: "
 
-    private val moduleReferenceSerializer = SModuleReferenceSerializer(repository)
+    private val modelSerializer = SModelSerializer(repository)
+    private val modelReferenceSerializer = SModelReferenceSerializer(repository)
 
-    // [SLanguage: [see SModuleReferenceSerializer.serialize(module)]
-    override fun serialize(it: SLanguage): String {
-        val moduleReference = it.sourceModuleReference
-        val serializedModuleReference = moduleReferenceSerializer.serialize(moduleReference)
-        return this.serialize(serializedModuleReference)
+    // [ModelWithModelReference: [see SModelSerializer.serialize(model)], modelReference: [see SModelReferenceSerializer.serialize(modelReference)]]
+    override fun serialize(it: ModelWithModelReference): String {
+        val serializedModel = modelSerializer.serialize(it.source)
+        return serializeWithRhs(serializedModel, it, it.modelReference, modelReferenceSerializer)
     }
 
-    override fun deserialize(from: String): SLanguage {
-        val serializedModuleReference = extractValue(from)
-        val moduleReference = moduleReferenceSerializer.deserializeNotNull(serializedModuleReference)
-        return MetaAdapterFactory.getLanguage(moduleReference)
+    override fun deserialize(from: String): ModelWithModelReference {
+        val split = splitIntoLhsAndRhs(from)
+
+        val model = modelSerializer.deserializeNotNull(split[0])
+        val modelReference = modelReferenceSerializer.deserializeNotNull(split[1])
+
+        return ModelWithModelReference(model, modelReference)
+    }
+}
+
+@UnstableModelixFeature(reason = "The new modelix MPS plugin is under construction", intendedFinalization = "2024.1")
+internal class ModelWithModuleReferenceSerializer(repository: SRepository? = null) :
+    Serializer<ModelWithModuleReference>() {
+
+    override val typeDelimiter = "ModelWithModuleReference"
+    override val infixSeparator = ", moduleReference: "
+
+    private val modelSerializer = SModelSerializer(repository)
+    private val moduleReferenceSerializer = SModuleReferenceSerializer(repository)
+
+    // [ModelWithModuleReference: [see SModelSerializer.serialize(model)], moduleReference: [see SModuleReferenceSerializer.serialize(moduleReference)]]
+    override fun serialize(it: ModelWithModuleReference): String {
+        val serializedModel = modelSerializer.serialize(it.source)
+        return serializeWithRhs(serializedModel, it, it.moduleReference, moduleReferenceSerializer)
+    }
+
+    override fun deserialize(from: String): ModelWithModuleReference {
+        val split = splitIntoLhsAndRhs(from)
+
+        val model = modelSerializer.deserializeNotNull(split[0])
+        val moduleReference = moduleReferenceSerializer.deserializeNotNull(split[1])
+
+        return ModelWithModuleReference(model, moduleReference)
+    }
+}
+
+@UnstableModelixFeature(reason = "The new modelix MPS plugin is under construction", intendedFinalization = "2024.1")
+internal class ModuleWithModuleReferenceSerializer(repository: SRepository? = null) :
+    Serializer<ModuleWithModuleReference>() {
+
+    override val typeDelimiter = "ModuleWithModuleReference"
+    override val infixSeparator = ", moduleReference: "
+
+    private val moduleSerializer = SModuleSerializer(repository)
+    private val moduleReferenceSerializer = SModuleReferenceSerializer(repository)
+
+    // [ModuleWithModuleReference: [see SModuleReference.serialize(model)], moduleReference: [see SModuleReferenceSerializer.serialize(moduleReference)]]
+    override fun serialize(it: ModuleWithModuleReference): String {
+        val serializedModule = moduleSerializer.serialize(it.source)
+        return serializeWithRhs(serializedModule, it, it.moduleReference, moduleReferenceSerializer)
+    }
+
+    override fun deserialize(from: String): ModuleWithModuleReference {
+        val split = splitIntoLhsAndRhs(from)
+
+        val module = moduleSerializer.deserializeNotNull(split[0])
+        val moduleReference = moduleReferenceSerializer.deserializeNotNull(split[1])
+
+        return ModuleWithModuleReference(module, moduleReference)
     }
 }
