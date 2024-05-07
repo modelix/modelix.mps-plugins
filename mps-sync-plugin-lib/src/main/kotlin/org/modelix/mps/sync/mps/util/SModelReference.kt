3ad21adbdafffd75f993ddaccebb4deb0fc1f69b
Benedek Horváth <bhorvath@itemis.com>
2024-05-07 11:14:43 +0200

    fix(mps-sync-plugin-lib): get the model and module references from the model
    instead of synthetically recreating them

--- mps-sync-plugin-lib/src/main/kotlin/org/modelix/mps/sync/mps/util/SModelReference.kt
+++
@@ -1,8 +0,0 @@
-package org.modelix.mps.sync.mps.util
-
-import org.jetbrains.mps.openapi.model.SModelReference
-import org.modelix.kotlin.utils.UnstableModelixFeature
-
-@UnstableModelixFeature(reason = "The new modelix MPS plugin is under construction", intendedFinalization = "2024.1")
-internal fun SModelReference.clone() =
-    jetbrains.mps.smodel.SModelReference(this.moduleReference?.clone(), this.modelId, this.modelName)
--- mps-sync-plugin-lib/src/main/kotlin/org/modelix/mps/sync/mps/util/SModuleReference.kt
+++ mps-sync-plugin-lib/src/main/kotlin/org/modelix/mps/sync/mps/util/SModuleReference.kt
@@ -1,11 +1,7 @@
 package org.modelix.mps.sync.mps.util
 
-import jetbrains.mps.project.structure.modules.ModuleReference
 import org.jetbrains.mps.openapi.module.SModuleReference
 import org.modelix.kotlin.utils.UnstableModelixFeature
 
 @UnstableModelixFeature(reason = "The new modelix MPS plugin is under construction", intendedFinalization = "2024.1")
 internal fun SModuleReference.getModelixId() = this.moduleId.toString()
-
-@UnstableModelixFeature(reason = "The new modelix MPS plugin is under construction", intendedFinalization = "2024.1")
-internal fun SModuleReference.clone() = ModuleReference(this.moduleName, this.moduleId)
