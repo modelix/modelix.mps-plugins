9ed70b53f84f48537d0c5a86039d2e9a0a201ed1
Benedek Horváth <bhorvath@itemis.com>
2024-06-11 16:14:00 +0200

    fix(mps-sync-plugin-lib): remove useless extra read transaction

--- mps-sync-plugin-lib/src/main/kotlin/org/modelix/mps/sync/transformation/modelixToMps/initial/ITreeToSTreeTransformer.kt
+++ mps-sync-plugin-lib/src/main/kotlin/org/modelix/mps/sync/transformation/modelixToMps/initial/ITreeToSTreeTransformer.kt
@@ -30,9 +30,11 @@ import org.modelix.mps.sync.transformation.modelixToMps.transformers.ModuleTrans
 import org.modelix.mps.sync.util.isModule
 import org.modelix.mps.sync.util.nodeIdAsLong
 import java.util.concurrent.CompletableFuture
-import java.util.concurrent.atomic.AtomicReference
 
-@UnstableModelixFeature(reason = "The new modelix MPS plugin is under construction", intendedFinalization = "This feature is finalized when the new sync plugin is ready for release.")
+@UnstableModelixFeature(
+    reason = "The new modelix MPS plugin is under construction",
+    intendedFinalization = "This feature is finalized when the new sync plugin is ready for release.",
+)
 class ITreeToSTreeTransformer(private val branch: IBranch, mpsLanguageRepository: MPSLanguageRepository) {
 
     private val moduleTransformer = ModuleTransformer(branch, mpsLanguageRepository)
@@ -41,16 +43,11 @@ class ITreeToSTreeTransformer(private val branch: IBranch, mpsLanguageRepository
 
     fun transform(moduleId: String): Iterable<IBinding> {
         val result = syncQueue.enqueue(linkedSetOf(SyncLock.MODELIX_READ), SyncDirection.NONE) {
-            val result = AtomicReference<INode>()
-            branch.runRead {
-                val moduleNode = branch.getRootNode().allChildren.firstOrNull {
-                    it.getPropertyValue(BuiltinLanguages.MPSRepositoryConcepts.Module.id) == moduleId
-                }
-                requireNotNull(moduleNode) { "Module node with ID '$moduleId' is not found on the root level." }
-                require(moduleNode.isModule()) { "Transformation entry point (Node $moduleNode) must be a Module." }
-                result.set(moduleNode)
+            val moduleNode = branch.getRootNode().allChildren.firstOrNull {
+                it.getPropertyValue(BuiltinLanguages.MPSRepositoryConcepts.Module.id) == moduleId
             }
-            result.get()
+            requireNotNull(moduleNode) { "Module node with ID '$moduleId' is not found on the root level." }
+            require(moduleNode.isModule()) { "Transformation entry point (Node $moduleNode) must be a Module." }
         }.continueWith(linkedSetOf(SyncLock.MODELIX_READ), SyncDirection.NONE) {
             val entryNodeId = (it as INode).nodeIdAsLong()
             moduleTransformer.transformToModuleCompletely(entryNodeId, true).getResult()
