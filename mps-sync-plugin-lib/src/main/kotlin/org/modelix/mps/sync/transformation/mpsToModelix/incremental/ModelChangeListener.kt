e75183105462c41b45d344fae0338af55b687b7b
Benedek Horváth <bhorvath@itemis.com>
2024-10-16 10:49:11 +0200

    docs(mps-sync-plugin-lib): document org.modelix.mps.sync.transformation.modelixToMps.incremental

--- mps-sync-plugin-lib/src/main/kotlin/org/modelix/mps/sync/transformation/mpsToModelix/incremental/ModelChangeListener.kt
+++ mps-sync-plugin-lib/src/main/kotlin/org/modelix/mps/sync/transformation/mpsToModelix/incremental/ModelChangeListener.kt
@@ -22,6 +22,7 @@ import jetbrains.mps.smodel.event.SModelDevKitEvent
 import jetbrains.mps.smodel.event.SModelImportEvent
 import jetbrains.mps.smodel.event.SModelLanguageEvent
 import jetbrains.mps.smodel.event.SModelListener
+import jetbrains.mps.smodel.event.SModelListener.SModelListenerPriority.CLIENT
 import jetbrains.mps.smodel.event.SModelPropertyEvent
 import jetbrains.mps.smodel.event.SModelReferenceEvent
 import jetbrains.mps.smodel.event.SModelRenamedEvent
@@ -29,17 +30,30 @@ import jetbrains.mps.smodel.event.SModelRootEvent
 import jetbrains.mps.smodel.loading.ModelLoadingState
 import org.jetbrains.mps.openapi.model.SModel
 import org.jetbrains.mps.openapi.model.SNode
+import org.jetbrains.mps.openapi.model.SNodeChangeListener
 import org.modelix.kotlin.utils.UnstableModelixFeature
 import org.modelix.model.api.BuiltinLanguages
 import org.modelix.model.api.IBranch
 import org.modelix.model.api.INode
 import org.modelix.mps.sync.bindings.ModelBinding
+import org.modelix.mps.sync.mps.services.ProjectLifecycleTracker
 import org.modelix.mps.sync.mps.services.ServiceLocator
+import org.modelix.mps.sync.tasks.ContinuableSyncTask
 import org.modelix.mps.sync.tasks.SyncDirection
 import org.modelix.mps.sync.tasks.SyncLock
 import org.modelix.mps.sync.transformation.mpsToModelix.initial.ModelSynchronizer
 import org.modelix.mps.sync.transformation.mpsToModelix.initial.NodeSynchronizer
 
+/**
+ * The change listener that is called, when a change on in an [SModel] in MPS occurred. This change will be played onto
+ * the model server MPS in a way that the MPS elements are transformed to modelix elements by the corresponding
+ * transformer methods.
+ *
+ * @param branch the modelix branch we are connected to.
+ * @param serviceLocator a collector class to simplify injecting the commonly used services in the sync plugin.
+ *
+ * @property binding the active [ModelBinding] of the [SModel] to which this change listener belongs.
+ */
 @UnstableModelixFeature(
     reason = "The new modelix MPS plugin is under construction",
     intendedFinalization = "This feature is finalized when the new sync plugin is ready for release.",
@@ -65,11 +79,27 @@ class ModelChangeListener(
      */
     private val nodeSynchronizer = NodeSynchronizer(branch, serviceLocator = serviceLocator)
 
+    /**
+     * Handles a Model Import added event. The added Model Import should be synced to the model server.
+     *
+     * @param event contains the Model Import that has to be synced to the model server.
+     *
+     * @see [ModelSynchronizer.addModelImport].
+     * @see [SModelListener.importAdded].
+     */
     override fun importAdded(event: SModelImportEvent) {
         modelSynchronizer.addModelImport(event.model, event.modelUID)
             .continueWith(linkedSetOf(SyncLock.NONE), SyncDirection.NONE) { resolveModelImports().getResult() }
     }
 
+    /**
+     * Handles a Model Import removed event. The removed Model Import should be deleted from the model server as well.
+     *
+     * @param event contains the Model Import that has to be removed from the model server.
+     *
+     * @see [NodeSynchronizer.removeNode].
+     * @see [SModelListener.importRemoved].
+     */
     override fun importRemoved(event: SModelImportEvent) {
         nodeSynchronizer.removeNode(
             parentNodeIdProducer = { it[event.model]!! },
@@ -77,10 +107,27 @@ class ModelChangeListener(
         )
     }
 
+    /**
+     * Handles a Language Dependency added event. The added Language Dependency should be synced to the model server.
+     *
+     * @param event contains the Language Dependency that has to be synced to the model server.
+     *
+     * @see [ModelSynchronizer.addLanguageDependency].
+     * @see [SModelListener.languageAdded].
+     */
     override fun languageAdded(event: SModelLanguageEvent) {
         modelSynchronizer.addLanguageDependency(event.model, event.eventLanguage)
     }
 
+    /**
+     * Handles a Language Dependency removed event. The removed Language Dependency should be deleted from the model
+     * server as well.
+     *
+     * @param event contains the Language Dependency that has to be removed from the model server.
+     *
+     * @see [NodeSynchronizer.removeNode].
+     * @see [SModelListener.languageRemoved].
+     */
     override fun languageRemoved(event: SModelLanguageEvent) {
         nodeSynchronizer.removeNode(
             parentNodeIdProducer = { it[event.model]!! },
@@ -88,10 +135,27 @@ class ModelChangeListener(
         )
     }
 
+    /**
+     * Handles a DevKit Dependency added event. The added DevKit Dependency should be synced to the model server.
+     *
+     * @param event contains the DevKit Dependency that has to be synced to the model server.
+     *
+     * @see [ModelSynchronizer.addLanguageDependency].
+     * @see [SModelListener.devkitAdded].
+     */
     override fun devkitAdded(event: SModelDevKitEvent) {
         modelSynchronizer.addDevKitDependency(event.model, event.devkitNamespace)
     }
 
+    /**
+     * Handles a DevKit Dependency removed event. The removed DevKit Dependency should be deleted from the model server
+     * as well.
+     *
+     * @param event contains the Language Dependency that has to be removed from the model server.
+     *
+     * @see [NodeSynchronizer.removeNode].
+     * @see [SModelListener.devkitRemoved].
+     */
     override fun devkitRemoved(event: SModelDevKitEvent) {
         nodeSynchronizer.removeNode(
             parentNodeIdProducer = { it[event.model]!! },
@@ -99,6 +163,14 @@ class ModelChangeListener(
         )
     }
 
+    /**
+     * Handles a model renamed event. The model has to be renamed on the model server as well.
+     *
+     * @param event contains the [SModel] that has to be renamed on the model server.
+     *
+     * @see [NodeSynchronizer.setProperty].
+     * @see [SModelListener.modelRenamed].
+     */
     override fun modelRenamed(event: SModelRenamedEvent) {
         nodeSynchronizer.setProperty(
             BuiltinLanguages.jetbrains_mps_lang_core.INamedConcept.name,
@@ -106,49 +178,127 @@ class ModelChangeListener(
         ) { it[event.model]!! }
     }
 
+    /**
+     * Handles the before model disposed event. This method is called, if the [model] is about to be disposed, therefore
+     * we have to do some precautionary clean-up activities.
+     *
+     * In our case, we deactivate the [binding] and remove the model from the model server iff the active [Project] is
+     * not closing. That's because the user removed the [model] from the [Project], thus we have to remove it on the
+     * model server too.
+     *
+     * @param model the [SModel] that will be disposed in MPS.
+     *
+     * @see [ProjectLifecycleTracker.projectClosing].
+     * @see [ModelBinding.deactivate].
+     * @see [SModelListener.beforeModelDisposed].
+     */
     override fun beforeModelDisposed(model: SModel) {
         if (!projectLifecycleTracker.projectClosing) {
             binding.deactivate(removeFromServer = true)
         }
     }
 
-    override fun getPriority(): SModelListener.SModelListenerPriority = SModelListener.SModelListenerPriority.CLIENT
+    /**
+     * @return [CLIENT].
+     *
+     * @see [SModelListener.getPriority].
+     */
+    override fun getPriority() = CLIENT
 
+    /**
+     * Does nothing, because it is a duplicate of [SNodeChangeListener.nodeAdded].
+     *
+     * @see [SModelListener.rootAdded].
+     */
     @Deprecated("Deprecated in Java")
     override fun rootAdded(event: SModelRootEvent) {
-        // duplicate of SNodeChangeListener.nodeAdded
     }
 
+    /**
+     * Does nothing, because it is a duplicate of [SNodeChangeListener.nodeRemoved].
+     *
+     * @see [SModelListener.rootRemoved].
+     */
     @Deprecated("Deprecated in Java")
     override fun rootRemoved(event: SModelRootEvent) {
-        // duplicate of SNodeChangeListener.nodeRemoved
     }
 
-    override fun propertyChanged(event: SModelPropertyEvent) {
-        // duplicate of SNodeChangeListener.propertyChanged
-    }
+    /**
+     * Does nothing, because it is a duplicate of [SNodeChangeListener.propertyChanged].
+     *
+     * @see [SModelListener.propertyChanged].
+     */
+    override fun propertyChanged(event: SModelPropertyEvent) {}
 
-    override fun childAdded(event: SModelChildEvent) {
-        // duplicate of SNodeChangeListener.childAdded
-    }
+    /**
+     * Does nothing, because it is a duplicate of [SNodeChangeListener.nodeAdded].
+     *
+     * @see [SModelListener.childAdded].
+     */
+    override fun childAdded(event: SModelChildEvent) {}
 
-    override fun childRemoved(event: SModelChildEvent) {
-        // duplicate of SNodeChangeListener.nodeRemoved
-    }
+    /**
+     * Does nothing, because it is a duplicate of [SNodeChangeListener.nodeRemoved].
+     *
+     * @see [SModelListener.childRemoved].
+     */
+    override fun childRemoved(event: SModelChildEvent) {}
 
-    override fun referenceAdded(event: SModelReferenceEvent) {
-        // duplicate of SNodeChangeListener.referenceChanged
-    }
+    /**
+     * Does nothing, because it is a duplicate of [SNodeChangeListener.referenceChanged].
+     *
+     * @see [SModelListener.referenceAdded].
+     */
+    override fun referenceAdded(event: SModelReferenceEvent) {}
 
-    override fun referenceRemoved(event: SModelReferenceEvent) {
-        // duplicate of SNodeChangeListener.referenceChanged
-    }
+    /**
+     * Does nothing, because it is a duplicate of [SNodeChangeListener.referenceChanged].
+     *
+     * @see [SModelListener.referenceRemoved].
+     */
+    override fun referenceRemoved(event: SModelReferenceEvent) {}
 
+    /**
+     * Does nothing.
+     *
+     * @see [SModelListener.beforeChildRemoved].
+     */
     override fun beforeChildRemoved(event: SModelChildEvent) {}
+
+    /**
+     * Does nothing.
+     *
+     * @see [SModelListener.beforeRootRemoved].
+     */
     override fun beforeRootRemoved(event: SModelRootEvent) {}
+
+    /**
+     * Does nothing.
+     *
+     * @see [SModelListener.beforeModelRenamed].
+     */
     override fun beforeModelRenamed(event: SModelRenamedEvent) {}
+
+    /**
+     * Does nothing.
+     *
+     * @see [SModelListener.modelSaved].
+     */
     override fun modelSaved(model: SModel) {}
+
+    /**
+     * Does nothing.
+     *
+     * @see [SModelListener.modelLoadingStateChanged].
+     */
     override fun modelLoadingStateChanged(model: SModel?, state: ModelLoadingState) {}
 
+    /**
+     * Resolves the Model Imports in [modelSynchronizer].
+     *
+     * @see [ModelSynchronizer.resolveModelImportsInTask].
+     *
+     * @return the [ContinuableSyncTask] handle to append a new sync task after this one is completed.
+     */
     fun resolveModelImports() = modelSynchronizer.resolveModelImportsInTask()
 }
--- mps-sync-plugin-lib/src/main/kotlin/org/modelix/mps/sync/transformation/mpsToModelix/incremental/NodeChangeListener.kt
+++ mps-sync-plugin-lib/src/main/kotlin/org/modelix/mps/sync/transformation/mpsToModelix/incremental/NodeChangeListener.kt
@@ -29,6 +29,14 @@ import org.modelix.model.mpsadapters.MPSProperty
 import org.modelix.mps.sync.mps.services.ServiceLocator
 import org.modelix.mps.sync.transformation.mpsToModelix.initial.NodeSynchronizer
 
+/**
+ * The change listener that is called, when a change on in an [SNode] in MPS occurred. This change will be played onto
+ * the model server MPS in a way that the MPS elements are transformed to modelix elements by the corresponding
+ * transformer methods.
+ *
+ * @param branch the modelix branch we are connected to.
+ * @param serviceLocator a collector class to simplify injecting the commonly used services in the sync plugin.
+ */
 @UnstableModelixFeature(
     reason = "The new modelix MPS plugin is under construction",
     intendedFinalization = "This feature is finalized when the new sync plugin is ready for release.",
@@ -40,10 +48,26 @@ class NodeChangeListener(branch: IBranch, serviceLocator: ServiceLocator) : SNod
      */
     private val synchronizer = NodeSynchronizer(branch, serviceLocator = serviceLocator)
 
+    /**
+     * Handles an [SNode] added event. The added [SNode] should be synced to the model server.
+     *
+     * @param event contains the [SNode] that has to be synced to the model server.
+     *
+     * @see [NodeSynchronizer.addNode].
+     * @see [SNodeChangeListener.nodeAdded].
+     */
     override fun nodeAdded(event: SNodeAddEvent) {
         synchronizer.addNode(event.child)
     }
 
+    /**
+     * Handles an [SNode] removed event. The removed [SNode] should be removed from the model server.
+     *
+     * @param event contains the [SNode] that has to be removed from the model server.
+     *
+     * @see [NodeSynchronizer.removeNode].
+     * @see [SNodeChangeListener.nodeRemoved].
+     */
     override fun nodeRemoved(event: SNodeRemoveEvent) {
         synchronizer.removeNode(
             parentNodeIdProducer = {
@@ -57,10 +81,27 @@ class NodeChangeListener(branch: IBranch, serviceLocator: ServiceLocator) : SNod
         )
     }
 
+    /**
+     * Handles a property changed event of an [SNode].
+     *
+     * @param event contains the [SNode], the property and its new value that have to be synced to the model server.
+     *
+     * @see [NodeSynchronizer.setProperty].
+     * @see [SNodeChangeListener.propertyChanged].
+     */
     override fun propertyChanged(event: SPropertyChangeEvent) {
         synchronizer.setProperty(MPSProperty(event.property), event.newValue) { it[event.node]!! }
     }
 
+    /**
+     * Handles a reference changed event of an [SNode].
+     *
+     * @param event contains the [SNode], the reference and its new target node that have to be synced to the model
+     * server.
+     *
+     * @see [NodeSynchronizer.setReference].
+     * @see [SNodeChangeListener.referenceChanged].
+     */
     override fun referenceChanged(event: SReferenceChangeEvent) {
         // TODO fix me: it does not work correctly, if event.newValue.targetNode points to a node that is in a different model, that has not been synced yet to model server...
         synchronizer.setReference(event.associationLink, event.node, event.newValue?.targetNode)
