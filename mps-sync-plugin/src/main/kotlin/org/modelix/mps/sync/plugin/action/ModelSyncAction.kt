4c37642f546c1f1429eeb9746e8762068e703fc0
Benedek Horváth <bhorvath@itemis.com>
2024-10-17 09:19:34 +0200

    docs(mps-sync-plugin): document classes in org.modelix.mps.sync.plugin.action

--- mps-sync-plugin/src/main/kotlin/org/modelix/mps/sync/plugin/action/ModelSyncAction.kt
+++ mps-sync-plugin/src/main/kotlin/org/modelix/mps/sync/plugin/action/ModelSyncAction.kt
@@ -18,12 +18,18 @@ package org.modelix.mps.sync.plugin.action
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.DataKey
 import jetbrains.mps.extapi.model.SModelBase
 import mu.KotlinLogging
 import org.jetbrains.mps.openapi.model.SModel
 import org.modelix.kotlin.utils.UnstableModelixFeature
+import org.modelix.mps.sync.bindings.ModelBinding
 
+/**
+ * An MPS action to synchronize an [SModel] to the model server. The action is registered in the MPS UI and thereby
+ * is triggered by the user.
+ *
+ * @see [AnAction].
+ */
 @UnstableModelixFeature(
     reason = "The new modelix MPS plugin is under construction",
     intendedFinalization = "This feature is finalized when the new sync plugin is ready for release.",
@@ -31,16 +37,21 @@ import org.modelix.kotlin.utils.UnstableModelixFeature
 @Suppress("ComponentNotRegistered")
 object ModelSyncAction : AnAction("Synchronize Model to Server") {
 
-    val contextModel = DataKey.create<SModel>("MPS_Context_SModel")
-
     /**
      * Just a normal logger to log messages.
      */
     private val logger = KotlinLogging.logger {}
 
+    /**
+     * Synchronizes the [SModel] that is in the [event] to the model server and activates its [ModelBinding].
+     *
+     * @param event the event from MPS, to fetch the [SModel] from it.
+     *
+     * @see [AnAction.actionPerformed].
+     */
     override fun actionPerformed(event: AnActionEvent) =
         actionPerformedSafely(event, logger, "Model synchronization error occurred.") { serviceLocator ->
-            val model = event.getData(contextModel) as? SModelBase
+            val model = event.getData(getMpsContextModelDataKey()) as? SModelBase
             checkNotNull(model) { "Synchronization is not possible, because Model (${model?.name}) is not an SModelBase." }
             check(!model.isReadOnly) { "Synchronization action is not allowed on read-only Model (${model.name})." }
 
