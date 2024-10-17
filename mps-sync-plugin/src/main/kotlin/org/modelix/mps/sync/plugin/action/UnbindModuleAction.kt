95b49dbddb754794802e80d533fcea7d811f4d62
Benedek Horváth <bhorvath@itemis.com>
2024-10-17 09:19:34 +0200

    docs(mps-sync-plugin): document classes in org.modelix.mps.sync.plugin.action

--- mps-sync-plugin/src/main/kotlin/org/modelix/mps/sync/plugin/action/UnbindModuleAction.kt
+++ mps-sync-plugin/src/main/kotlin/org/modelix/mps/sync/plugin/action/UnbindModuleAction.kt
@@ -18,12 +18,20 @@ package org.modelix.mps.sync.plugin.action
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.DataKey
 import jetbrains.mps.project.AbstractModule
 import mu.KotlinLogging
+import org.jetbrains.mps.openapi.model.SModel
 import org.jetbrains.mps.openapi.module.SModule
 import org.modelix.kotlin.utils.UnstableModelixFeature
+import org.modelix.mps.sync.bindings.ModelBinding
+import org.modelix.mps.sync.bindings.ModuleBinding
 
+/**
+ * An MPS action to deactivate the [ModuleBinding] of an [SModel] and therefore stop the MPS Module's synchronization to
+ * the model server. The action is registered in the MPS UI and thereby is triggered by the user.
+ *
+ * @see [AnAction].
+ */
 @UnstableModelixFeature(
     reason = "The new modelix MPS plugin is under construction",
     intendedFinalization = "This feature is finalized when the new sync plugin is ready for release.",
@@ -31,16 +39,23 @@ import org.modelix.kotlin.utils.UnstableModelixFeature
 @Suppress("ComponentNotRegistered")
 object UnbindModuleAction : AnAction("Unbind Module") {
 
-    private val contextModule = DataKey.create<SModule>("MPS_Context_SModule")
-
     /**
      * Just a normal logger to log messages.
      */
     private val logger = KotlinLogging.logger {}
 
+    /**
+     * Deactivates the [ModuleBinding] of the [SModule] that is in the [event] and therefore stops the MPS Module's and
+     * its MPS Models' synchronization to the model server. I.e., it deactivates the contained MPS Models'
+     * [ModelBinding]s too.
+     *
+     * @param event the event from MPS, to fetch the [SModule] from it.
+     *
+     * @see [AnAction.actionPerformed].
+     */
     override fun actionPerformed(event: AnActionEvent) =
         actionPerformedSafely(event, logger, "Module unbind error occurred.") { serviceLocator ->
-            val module = event.getData(contextModule) as? AbstractModule
+            val module = event.getData(getMpsContextModuleDataKey()) as? AbstractModule
             checkNotNull(module) { "Unbinding is not possible, because Module (${module?.moduleName}) is not an AbstractModule." }
             check(!module.isReadOnly) { "Unbinding is not possible, because Module (${module.moduleName}) is read-only." }
 
