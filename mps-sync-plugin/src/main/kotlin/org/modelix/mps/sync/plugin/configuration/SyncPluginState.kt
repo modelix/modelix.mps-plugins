9beb0e091c29a84d947f806b90663b5203435ec7
Benedek Horváth <bhorvath@itemis.com>
2024-10-17 09:33:16 +0200

    docs(mps-sync-plugin): document classes in org.modelix.mps.sync.plugin.configuration

--- mps-sync-plugin/src/main/kotlin/org/modelix/mps/sync/plugin/configuration/SyncPluginState.kt
+++ mps-sync-plugin/src/main/kotlin/org/modelix/mps/sync/plugin/configuration/SyncPluginState.kt
@@ -26,6 +26,20 @@ import org.modelix.kotlin.utils.UnstableModelixFeature
 import org.modelix.mps.sync.persistence.PersistableState
 import org.modelix.mps.sync.persistence.PluginStatePersister
 
+/**
+ * It is responsible for automatically loading and persisting the sync plugin's state via the [PersistableState] class,
+ * using intellij's [PersistentStateComponent] interface, [Service] and [Project] lifecycle mechanism. The singleton
+ * instance of this class will be automatically created if you use the `project.service<SyncPluginState>()` call.
+ *
+ * The persisted [PersistableState] is saved in the opened [Project]'s '.mps' folder with the name
+ * [PluginStatePersister.DEFAULT_FILE_NAME]. The next time you open the [Project], the file will be loaded by MPS.
+ *
+ * @property project the active [Project] in MPS.
+ *
+ * @see [Service].
+ * @see [State].
+ * @see [PersistentStateComponent].
+ */
 @UnstableModelixFeature(
     reason = "The new modelix MPS plugin is under construction",
     intendedFinalization = "This feature is finalized when the new sync plugin is ready for release.",
@@ -38,13 +52,23 @@ import org.modelix.mps.sync.persistence.PluginStatePersister
 )
 class SyncPluginState(private val project: Project) : PersistentStateComponent<PersistableState> {
 
+    /**
+     * The latest state of the plugin we have loaded from its persisted state (file).
+     */
     var latestState: PersistableState? = null
         private set
 
+    /**
+     * @return the latest state of the plugin that contains all information needed to restore the active connections and
+     * bindings to the model server the next time we start MPS.
+     */
     override fun getState(): PersistableState {
         return PersistableState().fetchState(project)
     }
 
+    /**
+     * Sets [latestState] to [newState].
+     */
     override fun loadState(newState: PersistableState) {
         latestState = newState
     }
